91ba8760bf6c7ca95251c0bbdd4932e741abcad0
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-08 12:38:27 +0000

    feat: add expertise cards constants with service offerings

---
+++ src/app/data/constants/expertise.contants.ts
@@ -0,0 +1,27 @@
+export const EXPERTISE_CARDS = [
+    {
+        title: "Web Development",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
+        image: "/assets/images/web-development.webp"
+    },
+    {
+        title: "Mobile Application Development",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
+        image: "/assets/images/mobile-application.webp"
+    },
+    {
+        title: "UX/UI Design",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
+        image: "/assets/images/ui-ux.webp"
+    },
+    {
+        title: "Graphic Design",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
+        image: "/assets/images/graphics-design.webp"
+    },
+    {
+        title: "Video Editing",
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
+        image: "/assets/images/video-editing.webp"
+    }
+] as const;
