3eeb3c89f498d642317543c5a73824173a79345b
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-08 14:24:26 +0000

    chore: add CoreValues component to main app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { HeroSection } from './components/hero-section/hero-section';
 import { OurExpertise } from './components/our-expertise/our-expertise';
+import { CoreValues } from "./components/core-values/core-values";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, HeroSection, OurExpertise],
+  imports: [Header, HeroSection, OurExpertise, CoreValues],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
