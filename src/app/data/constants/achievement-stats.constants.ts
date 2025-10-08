069ef53589d4dd7aa3eeec47b9f3eaf959cf1048
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-08 07:36:33 +0000

    feat: add achievement stats constants for project metrics display

---
+++ src/app/data/constants/achievement-stats.constants.ts
@@ -0,0 +1,21 @@
+export const ACHIEVEMENT_STATS = [{
+    imgUrl: 'assets/icons/briefcase.svg',
+    alt: 'briefcase',
+    value: '150+',
+    name: 'Successful Projects'
+}, {
+    imgUrl: 'assets/icons/banknotes.svg',
+    alt: 'banknotes',
+    value: '$10,000',
+    name: 'Funding Secured'
+}, {
+    imgUrl: 'assets/icons/heart.svg',
+    alt: 'heart',
+    value: '98%',
+    name: 'Client Adoption'
+}, {
+    imgUrl: 'assets/icons/bolt.svg',
+    alt: 'bolt',
+    value: '100%',
+    name: 'Project Completion'
+}] as const;
