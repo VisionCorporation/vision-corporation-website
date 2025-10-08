d10f337caebd094cde2dbafcd9b4f2466d2a2441
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-08 07:36:13 +0000

    feat: add hero section component with achievement stats display

---
+++ src/app/components/hero-section/hero-section.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { ACHIEVEMENT_STATS } from '../../data/constants/achievement-stats.constants';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [NgOptimizedImage],
+  templateUrl: './hero-section.html',
+  styleUrl: './hero-section.css'
+})
+export class HeroSection {
+  public readonly achievementStats = ACHIEVEMENT_STATS;
+}
