19c538d3fd71ddde3e8e346bf3479edd91031387
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-08 12:40:30 +0000

    feat: add OurExpertise component to App imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { HeroSection } from './components/hero-section/hero-section';
-// import { OurExpertise } from './components/our-expertise/our-expertise';
+import { OurExpertise } from './components/our-expertise/our-expertise';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, HeroSection],
+  imports: [Header, HeroSection, OurExpertise],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
