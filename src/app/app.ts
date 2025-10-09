1ca702dcb33efe5417a3fc58bfdc20cb6e0b09db
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-09 12:06:16 +0000

    feat: add testimonials component to app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './components/header/header';
 import { HeroSection } from './components/hero-section/hero-section';
 import { OurExpertise } from './components/our-expertise/our-expertise';
 import { CoreValues } from "./components/core-values/core-values";
+import { Testimonials } from './components/testimonials/testimonials';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, HeroSection, OurExpertise, CoreValues],
+  imports: [Header, HeroSection, OurExpertise, CoreValues, Testimonials],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
