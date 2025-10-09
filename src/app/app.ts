6ca4cd112cddad634887d83ba8242d00ec82e81f
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-09 14:16:25 +0000

    feat: integrate reality section component into app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { HeroSection } from './components/hero-section/hero-section';
 import { OurExpertise } from './components/our-expertise/our-expertise';
 import { CoreValues } from "./components/core-values/core-values";
 import { Testimonials } from './components/testimonials/testimonials';
+import { RealitySection } from './components/reality-section/reality-section';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, HeroSection, OurExpertise, CoreValues, Testimonials],
+  imports: [Header, HeroSection, OurExpertise, CoreValues, Testimonials, RealitySection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
