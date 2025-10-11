71ad318616d9cf3aa8053307765eb08bdb73c743
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-11 14:00:46 +0000

    feat: integrate footer component into app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { CoreValues } from "./components/core-values/core-values";
 import { Testimonials } from './components/testimonials/testimonials';
 import { RealitySection } from './components/reality-section/reality-section';
 import { NewsletterSection } from './components/newsletter-section/newsletter-section';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, HeroSection, OurExpertise, CoreValues, Testimonials, RealitySection, NewsletterSection],
+  imports: [Header, HeroSection, OurExpertise, CoreValues, Testimonials, RealitySection, NewsletterSection, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
