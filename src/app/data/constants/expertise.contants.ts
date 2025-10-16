c06cc19c4e324aa5497590cf3f05da0adc0a579d
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-16 12:26:17 +0000

    refactor: enhance mobile responsiveness and clean up expertise cards

--- src/app/data/constants/expertise.contants.ts
+++ src/app/data/constants/expertise.contants.ts
@@ -5,23 +5,8 @@ export const EXPERTISE_CARDS = [
         image: "/assets/images/web-development.webp"
     },
     {
-        title: "Mobile Application Development",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
-        image: "/assets/images/mobile-application.webp"
-    },
-    {
         title: "UX/UI Design",
         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
         image: "/assets/images/ui-ux.webp"
     },
-    {
-        title: "Graphic Design",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
-        image: "/assets/images/graphics-design.webp"
-    },
-    {
-        title: "Video Editing",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo. Praesent mattis commodo augue.",
-        image: "/assets/images/video-editing.webp"
-    }
 ] as const;
