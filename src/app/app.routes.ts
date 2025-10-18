09c29260a86f213d11439c1830868ed238c757c9
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-18 16:01:28 +0000

    chore: remove home route redirect and add _redirects file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadComponent: () => import("./pages/home/home").then((c) => c.Home) },
+    { path: '', loadComponent: () => import("./pages/home/home").then((c) => c.Home) },
     { path: "**", loadComponent: () => import("./pages/not-found/not-found").then((c) => c.NotFound) }
 ];
