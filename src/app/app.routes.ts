26f1b1836bd8863e71080280f3c8ea6b31ebd158
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-18 15:13:56 +0000

    chore: add routing configuration with home page and 404 handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import("./pages/home/home").then((c) => c.Home) },
+    { path: "**", loadComponent: () => import("./pages/not-found/not-found").then((c) => c.NotFound) }
+];
