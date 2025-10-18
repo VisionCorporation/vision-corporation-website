8bedd2fad5611129fdf09d2455a3bbe700ec056e
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-18 15:17:07 +0000

    feat: add home page component with complete layout structure

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/shared/header/header';
+import { HeroSection } from './hero-section/hero-section';
+import { OurExpertise } from './our-expertise/our-expertise';
+import { CoreValues } from './core-values/core-values';
+import { Testimonials } from '../../components/testimonials/testimonials';
+import { RealitySection } from './reality-section/reality-section';
+import { NewsletterSection } from './newsletter-section/newsletter-section';
+import { Footer } from '../../components/shared/footer/footer';
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, HeroSection, OurExpertise, CoreValues, Testimonials, RealitySection, NewsletterSection, Footer],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
