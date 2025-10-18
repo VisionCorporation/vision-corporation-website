fcf977d9a04ec8cceee82d0fee1771bbe3d24a27
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-18 23:45:49 +0000

    chore: add services route with prerendering support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import("./pages/home/home").then((c) => c.Home) },
+    { path: 'services', loadComponent: () => import("./pages/services/services").then((c) => c.Services) },
     { path: 'not-found', loadComponent: () => import('./pages/not-found/not-found').then(c => c.NotFound) },
     { path: '**', redirectTo: 'not-found' },
 ];
