eba7d7a2acf63e336dd5b1ae708a0ba43f69cd6c
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-20 10:16:51 +0000

    chore: add terms-of-service route with prerendering support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
     { path: '', loadComponent: () => import("./pages/home/home").then((c) => c.Home) },
     { path: 'services', loadComponent: () => import("./pages/services/services").then((c) => c.Services) },
     { path: 'privacy-policy', loadComponent: () => import("./pages/privacy-policy/privacy-policy").then((c) => c.PrivacyPolicy) },
+    { path: 'terms-of-service', loadComponent: () => import("./pages/terms-of-service/terms-of-service").then((c) => c.TermsOfService) },
     { path: '**', loadComponent: () => import("./pages/not-found/not-found").then((c) => c.NotFound) },
 ];
