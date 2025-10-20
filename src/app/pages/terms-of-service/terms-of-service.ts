82adc0c16cd2aa710100d9fc7e9746893c13bad9
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-20 10:15:56 +0000

    feat: add Terms of Service page with responsive design and SEO optimization

---
+++ src/app/pages/terms-of-service/terms-of-service.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { Header } from '../../components/shared/header/header';
+import { Footer } from '../../components/shared/footer/footer';
+import { SeoService } from '../../services/seo-service';
+
+@Component({
+  selector: 'app-terms-of-service',
+  imports: [Header, Footer],
+  templateUrl: './terms-of-service.html',
+  styleUrl: './terms-of-service.css'
+})
+export class TermsOfService {
+  private readonly seoService = inject(SeoService);
+
+  constructor() {
+    this.seoService.updatePageSeo({
+      title: 'Terms of Service - Vision Corporation',
+      description: 'Read Vision Corporation’s Terms of Service to understand your rights, responsibilities, and our policies when using our digital products and IT solutions.',
+      url: 'https://visioncorporationafrica.netlify.app/terms-of-service',
+      image: 'https://visioncorporationafrica.netlify.app/assets/images/terms-of-service-og.jpeg'
+    });
+  }
+}
