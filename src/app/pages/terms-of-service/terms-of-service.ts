5b599e9e4a446da244d77b1ea692f19264113c5a
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-21 15:33:53 +0000

    chore: optimize the background image for various screen sizes

--- src/app/pages/terms-of-service/terms-of-service.ts
+++ src/app/pages/terms-of-service/terms-of-service.ts
@@ -2,15 +2,18 @@ import { Component, inject } from '@angular/core';
 import { Header } from '../../components/shared/header/header';
 import { Footer } from '../../components/shared/footer/footer';
 import { SeoService } from '../../services/seo-service';
+import ngSrcset from './../../../../public/assets/ngSrcset.json';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-terms-of-service',
-  imports: [Header, Footer],
+  imports: [Header, Footer, NgOptimizedImage],
   templateUrl: './terms-of-service.html',
   styleUrl: './terms-of-service.css'
 })
 export class TermsOfService {
   private readonly seoService = inject(SeoService);
+  public srcSet = ngSrcset
 
   constructor() {
     this.seoService.updatePageSeo({
