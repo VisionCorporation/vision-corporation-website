6043660a911750ea25c08f7413c7f6f5af81b4bc
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-22 21:50:59 +0000

    feat: add mobile nav bar

--- src/app/components/shared/header/header.ts
+++ src/app/components/shared/header/header.ts
@@ -1,5 +1,5 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Renderer2, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -9,5 +9,39 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.css'
 })
 export class Header {
+  public isMenuOpen = false;
+  public isClosing = false;
+  private renderer = inject(Renderer2);
 
+  public toggleMenu(): void {
+    if (this.isMenuOpen) {
+      this.closeMenu();
+    } else {
+      this.openMenu();
+    }
+  }
+
+  public openMenu(): void {
+    this.isMenuOpen = true;
+    this.isClosing = false;
+    this.updateBodyScroll();
+  }
+
+  public closeMenu(): void {
+    this.isClosing = true;
+
+    setTimeout(() => {
+      this.isMenuOpen = false;
+      this.isClosing = false;
+      this.updateBodyScroll();
+    }, 300);
+  }
+
+  private updateBodyScroll(): void {
+    if (this.isMenuOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+    }
+  }
 }
