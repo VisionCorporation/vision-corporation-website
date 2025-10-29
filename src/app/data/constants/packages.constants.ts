a84d9c1e2f679ba6423b3b87164c24ee7e4c25e3
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-29 18:36:39 +0000

    feat: add package constants

---
+++ src/app/data/constants/packages.constants.ts
@@ -0,0 +1,88 @@
+export const PACKAGES = [{
+    title: "Full Stack Web Development Packages",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque, dolor nec feugiat ornare, quam sem vehicula tortor, eu finibus libero elit nec orci. Vivamus non ligula enim. Curabitur vel ultricies magna. Cras nec sem commodo, aliquam velit ut, convallis ",
+    types: [{
+        name: "Basic Package",
+        description: "Great for people just starting out, new businesses, or small companies that want a simple online  presence.",
+        specifications: ['Pre made design', ' Starter(1-4) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', '3 months free basic support and maintenance'],
+        price: 500
+    },
+    {
+        name: "Standard Package",
+        description: "Work well for small to medium-sized businesses, bloggers, and professionals who wants control over their site’s design with a simple online store or blog and a strong online presence.",
+        specifications: ['Custom design', 'Standard (5-8) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', 'Google Business Listing', 'Simple Online Store Feature ', 'Email Marketing', '6 months free basic support and maintenance'],
+        price: 5000
+    },
+    {
+        name: "Premium Package",
+        description: "Any business that wants to sell online needs an e-commerce package. These packages help you show off your products, take payments safely, and run your online store smoothly.",
+        specifications: ['Custom design', 'Premium 15+ pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Content Writing', 'Custom animation', 'Search Engine optimazation', 'Google Business Listing', 'Advance Online Store feature', 'Email Marketing', '12 months free basic support and maintenance '],
+        price: 10000
+    }]
+},
+{
+    title: "UX/UI Design Packages",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque, dolor nec feugiat ornare, quam sem vehicula tortor, eu finibus libero elit nec orci. Vivamus non ligula enim. Curabitur vel ultricies magna. Cras nec sem commodo, aliquam velit ut, convallis ",
+    types: [{
+        name: "Basic Package",
+        description: "Great for people just starting out, new businesses, or small companies that want a simple online  presence.",
+        specifications: ['Pre made design', ' Starter(1-4) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', '3 months free basic support and maintenance'],
+        price: 500
+    },
+    {
+        name: "Standard Package",
+        description: "Work well for small to medium-sized businesses, bloggers, and professionals who wants control over their site’s design with a simple online store or blog and a strong online presence.",
+        specifications: ['Custom design', 'Standard (5-8) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', 'Google Business Listing', 'Simple Online Store Feature ', 'Email Marketing', '6 months free basic support and maintenance'],
+        price: 5000
+    },
+    {
+        name: "Premium Package",
+        description: "Any business that wants to sell online needs an e-commerce package. These packages help you show off your products, take payments safely, and run your online store smoothly.",
+        specifications: ['Custom design', 'Premium 15+ pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Content Writing', 'Custom animation', 'Search Engine optimazation', 'Google Business Listing', 'Advance Online Store feature', 'Email Marketing', '12 months free basic support and maintenance '],
+        price: 10000
+    }]
+},
+{
+    title: "Frontend Development Packages",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque, dolor nec feugiat ornare, quam sem vehicula tortor, eu finibus libero elit nec orci. Vivamus non ligula enim. Curabitur vel ultricies magna. Cras nec sem commodo, aliquam velit ut, convallis ",
+    types: [{
+        name: "Basic Package",
+        description: "Great for people just starting out, new businesses, or small companies that want a simple online  presence.",
+        specifications: ['Pre made design', ' Starter(1-4) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', '3 months free basic support and maintenance'],
+        price: 500
+    },
+    {
+        name: "Standard Package",
+        description: "Work well for small to medium-sized businesses, bloggers, and professionals who wants control over their site’s design with a simple online store or blog and a strong online presence.",
+        specifications: ['Custom design', 'Standard (5-8) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', 'Google Business Listing', 'Simple Online Store Feature ', 'Email Marketing', '6 months free basic support and maintenance'],
+        price: 5000
+    },
+    {
+        name: "Premium Package",
+        description: "Any business that wants to sell online needs an e-commerce package. These packages help you show off your products, take payments safely, and run your online store smoothly.",
+        specifications: ['Custom design', 'Premium 15+ pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Content Writing', 'Custom animation', 'Search Engine optimazation', 'Google Business Listing', 'Advance Online Store feature', 'Email Marketing', '12 months free basic support and maintenance '],
+        price: 10000
+    }]
+},
+{
+    title: "Backend Development Packages",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pellentesque, dolor nec feugiat ornare, quam sem vehicula tortor, eu finibus libero elit nec orci. Vivamus non ligula enim. Curabitur vel ultricies magna. Cras nec sem commodo, aliquam velit ut, convallis ",
+    types: [{
+        name: "Basic Package",
+        description: "Great for people just starting out, new businesses, or small companies that want a simple online  presence.",
+        specifications: ['Pre made design', ' Starter(1-4) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', '3 months free basic support and maintenance'],
+        price: 500
+    },
+    {
+        name: "Standard Package",
+        description: "Work well for small to medium-sized businesses, bloggers, and professionals who wants control over their site’s design with a simple online store or blog and a strong online presence.",
+        specifications: ['Custom design', 'Standard (5-8) pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Search Engine optimazation', 'Google Business Listing', 'Simple Online Store Feature ', 'Email Marketing', '6 months free basic support and maintenance'],
+        price: 5000
+    },
+    {
+        name: "Premium Package",
+        description: "Any business that wants to sell online needs an e-commerce package. These packages help you show off your products, take payments safely, and run your online store smoothly.",
+        specifications: ['Custom design', 'Premium 15+ pages', 'Development of page with React or Angular', 'Content Management System', 'Custom domain', 'Hosting', 'Content Writing', 'Custom animation', 'Search Engine optimazation', 'Google Business Listing', 'Advance Online Store feature', 'Email Marketing', '12 months free basic support and maintenance '],
+        price: 10000
+    }]
+}]
