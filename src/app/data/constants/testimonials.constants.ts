439d3e9eed6d2cb95fab0a54415b073df0283215
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-31 15:44:40 +0000

    fix(testimonials): update testimonial messages for improved clarity and impact

--- src/app/data/constants/testimonials.constants.ts
+++ src/app/data/constants/testimonials.constants.ts
@@ -2,14 +2,14 @@ export const TESTIMONIALS = [
     {
         userName: 'Godfred Adams',
         userImage: 'assets/images/adams-godfred.webp',
-        message: 'Vision Corporation transformed our business strategy and helped us secure $2M in funding within 6 months.'
+        message: 'Professional, responsive, and quality-driven — Vision Corporation made what seemed impossible achievable. We couldn’t have asked for a smoother experience.'
     },
     {
         userName: 'Kwesi Amissah',
         userImage: 'assets/images/adams-godfred.webp',
-        message: "Vision Corporation transformed our business strategy and helped us secure $2M in funding within 6 months."
+        message: "The attention to detail in their UI/UX process was remarkable; they not only delivered a beautiful design but also provided a clear, intuitive user experience that will significantly increase our product adoption rates.” - Project Stakeholder."
     }, {
         userName: 'Ebenezer Antwi',
         userImage: 'assets/images/adams-godfred.webp',
-        message: "Vision Corporation transformed our business strategy and helped us secure $2M in funding within 6 months."
+        message: "We were impressed by the speed and scalability of the final web solution. The development team architected the back-end flawlessly, assuring us that our platform can handle future growth without performance issues"
     }] as const;
