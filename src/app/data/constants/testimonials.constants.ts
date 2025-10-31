1fb4e31d0d0cc8e2aeab7b8300d6646a7898f4b0
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-31 16:28:36 +0000

    fix(testimonials): update testimonial entries for accuracy and clarity

--- src/app/data/constants/testimonials.constants.ts
+++ src/app/data/constants/testimonials.constants.ts
@@ -1,15 +1,15 @@
 export const TESTIMONIALS = [
     {
-        userName: 'Godfred Adams',
-        userImage: 'assets/images/adams-godfred.webp',
-        message: 'Professional, responsive, and quality-driven — Vision Corporation made what seemed impossible achievable. We couldn’t have asked for a smoother experience.'
+        userName: 'Gabriel Addai',
+        userImage: 'assets/images/gabriel.webp',
+        message: 'Professional, responsive, and quality driven. Vision Corporation made what seemed impossible achievable. We couldn’t have asked for a smoother experience.'
     },
     {
-        userName: 'Kwesi Amissah',
-        userImage: 'assets/images/adams-godfred.webp',
+        userName: 'Project Stakeholder',
+        userImage: 'assets/images/project-stakeholder.webp',
         message: "The attention to detail in their UI/UX process was remarkable; they not only delivered a beautiful design but also provided a clear, intuitive user experience that will significantly increase our product adoption rates.” - Project Stakeholder."
     }, {
-        userName: 'Ebenezer Antwi',
-        userImage: 'assets/images/adams-godfred.webp',
+        userName: 'Technical Lead',
+        userImage: 'assets/images/technical-lead.webp',
         message: "We were impressed by the speed and scalability of the final web solution. The development team architected the back-end flawlessly, assuring us that our platform can handle future growth without performance issues"
     }] as const;
