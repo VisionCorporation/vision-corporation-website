9561dca831f143924babbdd69b05b17ad671d524
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-10-31 17:38:48 +0000

    fix(testimonials): add image handling for testimonials and improve initials display

--- src/app/data/constants/testimonials.constants.ts
+++ src/app/data/constants/testimonials.constants.ts
@@ -1,14 +1,17 @@
 export const TESTIMONIALS = [
     {
+        hasImage: true,
         userName: 'Gabriel Addai',
         userImage: 'assets/images/gabriel.webp',
         message: 'Professional, responsive, and quality driven. Vision Corporation made what seemed impossible achievable. We couldn’t have asked for a smoother experience.'
     },
     {
+        hasImage: false,
         userName: 'Project Stakeholder',
         userImage: 'assets/images/project-stakeholder.webp',
         message: "The attention to detail in their UI/UX process was remarkable; they not only delivered a beautiful design but also provided a clear, intuitive user experience that will significantly increase our product adoption rates.” - Project Stakeholder."
     }, {
+        hasImage: false,
         userName: 'Technical Lead',
         userImage: 'assets/images/technical-lead.webp',
         message: "We were impressed by the speed and scalability of the final web solution. The development team architected the back-end flawlessly, assuring us that our platform can handle future growth without performance issues"
--- src/app/pages/home/testimonials/testimonials.ts
+++ src/app/pages/home/testimonials/testimonials.ts
@@ -10,4 +10,13 @@ import { TESTIMONIALS } from '../../../data/constants/testimonials.constants';
 })
 export class Testimonials {
   public readonly testimonials = TESTIMONIALS
+
+  public getInitials(name: string): string {
+    if (!name) return '';
+    return name
+      .split(' ')
+      .map(word => word.charAt(0))
+      .join('')
+      .toUpperCase();
+  }
 }
