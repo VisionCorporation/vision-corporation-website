ad2f2e4fce8fb569c3da55b72f12baf39bb170e4
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-11-03 21:57:33 +0000

    chore: add HTTP client provider with fetch support to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
         anchorScrolling: 'enabled'
-      })), provideClientHydration(withEventReplay())
+      })), provideClientHydration(withEventReplay()),
+      provideHttpClient(withFetch())
   ]
 };
