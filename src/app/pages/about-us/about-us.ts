bc11f74895b075d815c4a02144cda992f1951eec
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-11-11 00:24:30 +0000

    chore: add our journey component to the parent component (about-us)

--- src/app/pages/about-us/about-us.ts
+++ src/app/pages/about-us/about-us.ts
@@ -4,10 +4,11 @@ import { Footer } from '../../components/shared/footer/footer';
 import { NgOptimizedImage } from '@angular/common';
 import { WhoWeAre } from './who-we-are/who-we-are';
 import { OurVision } from './our-vision/our-vision';
+import { OurJourney } from './our-journey/our-journey';
 
 @Component({
   selector: 'app-about-us',
-  imports: [Header, Footer, WhoWeAre, OurVision],
+  imports: [Header, Footer, WhoWeAre, OurVision, OurJourney],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
