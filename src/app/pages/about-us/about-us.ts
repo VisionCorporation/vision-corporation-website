afb7778c582e72b782b5e7ed85800fc372fb2458
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-11-11 01:25:52 +0000

    chore: add routerLink to the imports array

--- src/app/pages/about-us/about-us.ts
+++ src/app/pages/about-us/about-us.ts
@@ -6,10 +6,11 @@ import { WhoWeAre } from './who-we-are/who-we-are';
 import { OurVision } from './our-vision/our-vision';
 import { OurJourney } from './our-journey/our-journey';
 import { OurPhilosophy } from './our-philosophy/our-philosophy';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
-  imports: [Header, Footer, WhoWeAre, OurVision, OurJourney, OurPhilosophy],
+  imports: [Header, Footer, WhoWeAre, OurVision, OurJourney, OurPhilosophy, RouterLink],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
