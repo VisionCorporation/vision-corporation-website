1f7f0523512a76907c6926c4b3a47c61861c5649
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-11-11 13:47:42 +0000

    chore: add our journey cards

--- src/app/pages/about-us/our-journey/our-journey.ts
+++ src/app/pages/about-us/our-journey/our-journey.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { JOURNEY_INFO } from '../../../data/constants/about-us.constants';
 
 @Component({
   selector: 'app-our-journey',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './our-journey.html',
   styleUrl: './our-journey.css'
 })
 export class OurJourney {
+  private readonly sanitizer = inject(DomSanitizer);
 
+  public readonly features = JOURNEY_INFO.map(card => ({
+    ...card,
+    svg: this.sanitizer.bypassSecurityTrustHtml(card.svg)
+  }));
 }
