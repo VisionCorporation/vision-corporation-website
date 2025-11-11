c82bc38823f6af2a767068094d02f84380f0b8d1
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-11-11 00:28:03 +0000

    chore: add our philosophy component to the parent component (about-us)

--- src/app/pages/about-us/about-us.ts
+++ src/app/pages/about-us/about-us.ts
@@ -5,10 +5,11 @@ import { NgOptimizedImage } from '@angular/common';
 import { WhoWeAre } from './who-we-are/who-we-are';
 import { OurVision } from './our-vision/our-vision';
 import { OurJourney } from './our-journey/our-journey';
+import { OurPhilosophy } from './our-philosophy/our-philosophy';
 
 @Component({
   selector: 'app-about-us',
-  imports: [Header, Footer, WhoWeAre, OurVision, OurJourney],
+  imports: [Header, Footer, WhoWeAre, OurVision, OurJourney, OurPhilosophy],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
