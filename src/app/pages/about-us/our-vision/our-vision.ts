431c3795998a5797066caf3114d80d5d6be35a3e
Ebenezer Antwi <antwiebenezer824@gmail.com>
2025-11-11 00:20:46 +0000

    feat: add our vision component

---
+++ src/app/pages/about-us/our-vision/our-vision.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-vision',
+  imports: [NgOptimizedImage],
+  templateUrl: './our-vision.html',
+  styleUrl: './our-vision.css'
+})
+export class OurVision {
+
+}
