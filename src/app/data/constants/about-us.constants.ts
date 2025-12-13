2ee29bbb667d9e257e05f70e5d60d71e289417b0
kwadwoambitious <antwiebenezer824@gmail.com>
2025-12-13 11:18:43 +0000

    chore: update descriptions in about us section for clarity and impact

--- src/app/data/constants/about-us.constants.ts
+++ src/app/data/constants/about-us.constants.ts
@@ -12,7 +12,7 @@ export const WHO_WE_ARE = [{
 </svg>
 `,
     title: 'Clear Mission',
-    description: 'Architecting infrastructure for equitable and sustainable development across the continent.'
+    description: 'To design, build, and scale practical, locally-led hardware and software solutions that address Africa’s most critical infrastructure challenges.'
 },
 {
     svg: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -64,7 +64,7 @@ export const JOURNEY_INFO = [
     {
         title: 'Software Development',
         description:
-            "Founded by visionary entrepreneurs committed to transforming Africa's technological landscape.",
+            "We begin with software excellence to fund and accelerate development.",
         iconBg: 'bg-[#EA690C]',
         svg: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M9 17.25V18.2574C9 19.053 8.68393 19.8161 8.12132 20.3787L7.5 21H16.5L15.8787 20.3787C15.3161 19.8161 15 19.053 15 18.2574V17.25M21 5.25V15C21 16.2426 19.9926 17.25 18.75 17.25H5.25C4.00736 17.25 3 16.2426 3 15V5.25M21 5.25C21 4.00736 19.9926 3 18.75 3H5.25C4.00736 3 3 4.00736 3 5.25M21 5.25V12C21 13.2426 19.9926 14.25 18.75 14.25H5.25C4.00736 14.25 3 13.2426 3 12V5.25" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
